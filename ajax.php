b4aad98655ec6621a9107ab9145aa9f603b5bed5
Arian <arian@aryweb.nl>
2010-11-14 13:56:07 +0100

    Disable caching for requests

--- ajax.php
+++ ajax.php
@@ -5,15 +5,20 @@ sleep($delay);
 
 
 if (!empty($_GET['html'])){
-	echo $_GET['html'];
+	$output = $_GET['html'];
 } elseif (!empty($_POST['html'])){
-	echo $_POST['html'];
+	$output = $_POST['html'];
 } elseif (!empty($_GET['jsonp'])){
 
 	header('Content-Type: text/javascript');
 	$callback = !empty($_GET['callback']) ? $_GET['callback'] : 'callback';
-	echo $callback . '({"some": "jsonp", "data": "data"});';
+	$output = $callback . '({"some": "jsonp", "data": "data"});';
 
 } else {
-	echo 'html';
+	$output = 'html';
 }
+
+header("Cache-Control: no-cache");
+header("Pragma: no-cache");
+
+echo $output;
